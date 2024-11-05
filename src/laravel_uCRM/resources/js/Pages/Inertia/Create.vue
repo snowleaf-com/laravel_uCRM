1e070e89a26440299580e4374318659b5561d4d4
snowleaf-com <yy.dec5@gmail.com>
2024-11-05 11:41:10 +0900

    Laravel側でバリデーション

--- src/laravel_uCRM/resources/js/Pages/Inertia/Create.vue
+++ src/laravel_uCRM/resources/js/Pages/Inertia/Create.vue
@@ -1,6 +1,11 @@
 <script setup>
   import { reactive } from 'vue';
   import { Inertia } from '@inertiajs/inertia';
+import { errorMessages } from 'vue/compiler-sfc';
+
+  defineProps({
+    errors: Object,
+  })
 
   const form = reactive({
     title: null,
@@ -15,7 +20,9 @@
 <template>
   <form v-on:submit.prevent="submitFunction">
     <input name="title" v-model="form.title"><br>
+    <div v-if="errors.title">{{ errors.title }}</div>
     <input name="content" v-model="form.content">
+    <div v-if="errors.content">{{ errors.content }}</div>
     <button>送信</button>
   </form>
 </template>
