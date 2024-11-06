3a0a82279cd86f77bd6b88f3401395538f19b92e
snowleaf-com <yy.dec5@gmail.com>
2024-11-07 07:10:40 +0900

    defineEmits

--- src/laravel_uCRM/resources/js/Pages/ComponentTest.vue
+++ src/laravel_uCRM/resources/js/Pages/ComponentTest.vue
@@ -2,11 +2,15 @@
 import GuestLayout from '@/Layouts/GuestLayout.vue';
 import Label from '@/Components/InputLabel.vue'
 import Input from '@/Components/TextInput.vue'
+
+const emitTest = e => {
+  console.log(e);
+}
 </script>
 
 <template>
   <GuestLayout>
     <Label value="件名">タイトル</Label>
-    <Input modelValue="初期値"/>
+    <Input @update:modelValue="emitTest" modelValue="初期値"/>
   </GuestLayout>
 </template>
