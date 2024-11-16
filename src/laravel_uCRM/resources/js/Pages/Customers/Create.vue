c22baf0825a9465c35182453efcc96bdd9b69fd4
snowleaf-com <yy.dec5@gmail.com>
2024-11-16 22:12:51 +0900

    郵便番号から住所自動入力

--- src/laravel_uCRM/resources/js/Pages/Customers/Create.vue
+++ src/laravel_uCRM/resources/js/Pages/Customers/Create.vue
@@ -4,10 +4,17 @@ import { reactive } from 'vue';
 import { Head, useForm } from '@inertiajs/vue3';
 import { Inertia } from '@inertiajs/inertia';
 import InputError from '@/Components/InputError.vue';
+import { Core as YubinBangoCore } from "yubinbango-core2";
 
 defineProps({
   errors: Object,
 })
+
+const fetchAddress = () => {
+  new YubinBangoCore(String(form.postcode), (value) => {
+    form.address = value.region + value.locality + value.street
+  })
+}
 
 const form = useForm({
   name: '',
@@ -69,7 +76,7 @@ const storeCustomer = () => {
                             <div class="p-2 w-full">
                               <div class="relative">
                                 <label for="postcode" class="leading-7 text-sm text-gray-600">郵便番号</label><InputError :message="errors.postcode" />
-                                <input type="number" id="postcode" name="postcode" v-model="form.postcode" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                <input type="number" id="postcode" name="postcode" v-model="form.postcode" @change="fetchAddress" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                               </div>
                             </div>
                             <div class="p-2 w-full">
