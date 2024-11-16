5c263992ceb83831117aebadf15f1c42cb2b55e8
snowleaf-com <yy.dec5@gmail.com>
2024-11-17 06:36:40 +0900

    引数の中身がない場合の対応

--- src/laravel_uCRM/resources/js/common.js
+++ src/laravel_uCRM/resources/js/common.js
@@ -1,6 +1,13 @@
 const nl2br = (str) => {
+    // str が null または undefined または 空文字列の場合、空文字列を返す
+    if (!str) {
+        return '';
+    }
+
+    // 改行コードを <br /> に置き換える
     str = str.replace(/\r\n/g, "<br />");
     str = str.replace(/(\n|\r)/g, "<br />");
+
     return str;
 }
 
