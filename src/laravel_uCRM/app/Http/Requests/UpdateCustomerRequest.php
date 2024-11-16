acf676bf69ca10bbcd90a9c1de299520dfa197d5
snowleaf-com <yy.dec5@gmail.com>
2024-11-17 06:40:51 +0900

    更新の場合、バリデーションチェックのuniqueを自身を含めないようにした

--- src/laravel_uCRM/app/Http/Requests/UpdateCustomerRequest.php
+++ src/laravel_uCRM/app/Http/Requests/UpdateCustomerRequest.php
@@ -13,7 +13,7 @@ class UpdateCustomerRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -23,8 +23,20 @@ class UpdateCustomerRequest extends FormRequest
      */
     public function rules()
     {
+        // リクエストから現在のレコードのIDを取得
+        $customerId = $this->route('customer')->id; // ルートパラメータから取得
+        // dd($customerId);
+
         return [
-            //
+            'name' => ['required', 'max:50'],
+            'kana' => ['required', 'regex:/^[ァ-ヾ 　]+$/u', 'max:50'],
+            'tel' => ['required', 'max:20', 'unique:customers,tel,' . $customerId],
+            'email' => ['required', 'email', 'max:255', 'unique:customers,email,' . $customerId],
+            'postcode' => ['required', 'max:7'],
+            'address' => ['required', 'max:100'],
+            'birthday' => ['date'],
+            'gender' => ['required'],
+            'memo' => ['max:1000'],
         ];
     }
 }
