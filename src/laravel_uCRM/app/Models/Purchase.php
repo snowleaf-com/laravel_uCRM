0003ac250aab4e3c214935d53c88d09a0a020eef
snowleaf-com <yy.dec5@gmail.com>
2024-11-19 06:38:35 +0900

    CustomerとPurchaseを１対多のリレーションを張る

--- src/laravel_uCRM/app/Models/Purchase.php
+++ src/laravel_uCRM/app/Models/Purchase.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Customer;
 
 class Purchase extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'customer_id',
+        'status'
+    ];
+
+    public function customer() {
+        return $this->belongsTo(Customer::class);
+    }
 }
