82b604eff2dea68d807c01056c8232b5ad45c356
snowleaf-com <yy.dec5@gmail.com>
2024-11-19 07:06:10 +0900

    中間テーブル紐づけダミーデータ

--- src/laravel_uCRM/database/seeders/DatabaseSeeder.php
+++ src/laravel_uCRM/database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Item;
 use Illuminate\Database\Seeder;
+use App\Models\Purchase;
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,7 +22,16 @@ class DatabaseSeeder extends Seeder
             ItemSeeder::class
         ]);
         \App\Models\Customer::factory(1000)->create();
-        \App\Models\Purchase::factory(100)->create();
+
+        $items = Item::all();
+
+        Purchase::factory(100)->create()->each(
+            function(Purchase $purchase) use ($items){
+                $purchase->items()->attach(
+                    $items->random(rand(1,3))->pluck('id')->toArray(),
+                    ['quantity' => rand(1, 5)]
+                );
+        });
 
         // \App\Models\User::factory(10)->create();
 
