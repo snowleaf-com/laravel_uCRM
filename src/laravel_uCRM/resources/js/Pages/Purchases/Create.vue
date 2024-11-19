3f5390a45eee794506f7f8959bff6f9235cfeff1
snowleaf-com <yy.dec5@gmail.com>
2024-11-20 05:51:33 +0900

    日付取得・会員情報取得して表示

--- src/laravel_uCRM/resources/js/Pages/Purchases/Create.vue
+++ src/laravel_uCRM/resources/js/Pages/Purchases/Create.vue
@@ -1,6 +1,31 @@
 <script setup>
+import { getToday } from '@/common';
+import { onMounted, reactive } from 'vue';
+
+onMounted(() => {
+  form.date = getToday()
+})
+
+const form = reactive({
+  date: null,
+  customer_id: null,
+})
+
+const props = defineProps({
+  'customers': Array,
+
+})
 </script>
 
 <template>
-  purchases/create
+  会員名
+  <br>
+  <select name="customer" v-model="form.customer_id">
+    <option v-for="customer in customers" :value="customer.id" :key="customer.id">
+      {{ customer.id }} : {{ customer.name }}
+    </option>
+  </select>
+  <br>
+  日付<br>
+  <input type="date" name="date" v-model="form.date">
 </template>
