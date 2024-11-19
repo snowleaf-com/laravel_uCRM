ec7b1be892e12a99ece57b7bc314542b5cecf6ab
snowleaf-com <yy.dec5@gmail.com>
2024-11-19 18:55:21 +0900

    購入画面・ルーティング設定

--- src/laravel_uCRM/routes/web.php
+++ src/laravel_uCRM/routes/web.php
@@ -7,6 +7,7 @@ use Inertia\Inertia;
 use App\Http\Controllers\InertiaTestController;
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\PurchaseController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +19,8 @@ use App\Http\Controllers\CustomerController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::resource('purchases', PurchaseController::class)->middleware(['auth', 'verified']);
 
 Route::resource('customers', CustomerController::class)->middleware(['auth', 'verified']);
 
