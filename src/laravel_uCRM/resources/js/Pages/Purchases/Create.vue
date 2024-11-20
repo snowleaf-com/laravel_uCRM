0cd9037cdd95a1948cf0b2b6c4d15f8417048f7c
snowleaf-com <yy.dec5@gmail.com>
2024-11-21 07:04:51 +0900

    購入処理（２つのテーブルに保存）

--- src/laravel_uCRM/resources/js/Pages/Purchases/Create.vue
+++ src/laravel_uCRM/resources/js/Pages/Purchases/Create.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { getToday } from '@/common';
 import { computed, onMounted, reactive, ref } from 'vue';
+import { Inertia } from '@inertiajs/inertia';
 
 const props = defineProps({
   'customers' : Array,
@@ -10,6 +11,8 @@ const props = defineProps({
 const form = reactive({
   date: null,
   customer_id: null,
+  status: true,
+  items: []
 })
 
 const itemList = ref([])
@@ -34,47 +37,61 @@ const totalPrice = computed(() => {
   return total;
 })
 
+const storePurchase = () => {
+  itemList.value.forEach( item => {
+    if( item.quantity > 0) {
+      form.items.push({
+        id: item.id,
+        quantity: item.quantity
+      })
+    } 
+  })
+  Inertia.post(route('purchases.store'), form)
+}
 
 
 </script>
 
 <template>
-  日付<br>
-  <input type="date" name="date" v-model="form.date"><br>
-  会員名
-  <br>
-  <select name="customer" v-model="form.customer_id">
-    <option v-for="customer in customers" :value="customer.id" :key="customer.id">
-      {{ customer.id }} : {{ customer.name }}
-    </option>
-  </select>
-  <br>
-  <table>
-    <thead>
-      <tr>
-        <th>ID</th>
-        <th>商品名</th>
-        <th>金額</th>
-        <th>数量</th>
-        <th>小計</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="item in itemList">
-        <td>{{ item.id }}</td>
-        <td>{{ item.name }}</td>
-        <td>{{ item.price }}</td>
-        <td>
-          <select name="quantity" v-model="item.quantity">
-            <option v-for="q in quantity" :value="q">
-              {{ q }}
-            </option>
-          </select>
-        </td>
-        <td>{{ item.price * item.quantity }}</td>
-      </tr>
-    </tbody>
-  </table>
-  <br>
-  合計: {{ totalPrice }}円
+  <form @submit.prevent="storePurchase">
+    日付<br>
+    <input type="date" name="date" v-model="form.date"><br>
+    会員名
+    <br>
+    <select name="customer" v-model="form.customer_id">
+      <option v-for="customer in customers" :value="customer.id" :key="customer.id">
+        {{ customer.id }} : {{ customer.name }}
+      </option>
+    </select>
+    <br>
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>商品名</th>
+          <th>金額</th>
+          <th>数量</th>
+          <th>小計</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in itemList">
+          <td>{{ item.id }}</td>
+          <td>{{ item.name }}</td>
+          <td>{{ item.price }}</td>
+          <td>
+            <select name="quantity" v-model="item.quantity">
+              <option v-for="q in quantity" :value="q">
+                {{ q }}
+              </option>
+            </select>
+          </td>
+          <td>{{ item.price * item.quantity }}</td>
+        </tr>
+      </tbody>
+    </table>
+    <br>
+    合計: {{ totalPrice }}円<br>
+    <button>登録する</button>
+  </form>
 </template>
