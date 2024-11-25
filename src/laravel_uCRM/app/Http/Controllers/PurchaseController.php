7ae3ac8c1548688d40e75d58b55d930d2e3e72d4
snowleaf-com <yy.dec5@gmail.com>
2024-11-25 16:01:58 +0900

    グローバルスコープにてOrderモデルからデータ取得

--- src/laravel_uCRM/app/Http/Controllers/PurchaseController.php
+++ src/laravel_uCRM/app/Http/Controllers/PurchaseController.php
@@ -9,6 +9,7 @@ use App\Models\Customer;
 use App\Models\Item;
 use Inertia\Inertia;
 use Illuminate\Support\Facades\DB;
+use App\Models\Order;
 
 class PurchaseController extends Controller
 {
@@ -19,7 +20,7 @@ class PurchaseController extends Controller
      */
     public function index()
     {
-        //
+        dd(Order::paginate(50));
     }
 
     /**
