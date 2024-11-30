10b67f4c3edde6b578ccec8d3838fb2f89d1e508
snowleaf-com <yy.dec5@gmail.com>
2024-11-30 20:59:26 +0900

    typo修正

--- src/laravel_uCRM/resources/js/Pages/Purchases/Show.vue
+++ src/laravel_uCRM/resources/js/Pages/Purchases/Show.vue
@@ -36,7 +36,7 @@ onMounted(() => {
                             <div class="p-2 w-full">
                               <div class="relative">
                                 <label for="name" class="leading-7 text-sm text-gray-600">日付</label>
-                                <div id="date" name="date" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">{{ dayjs(props.order[0].create_at).format('YYYY-MM-DD') }}</div>
+                                <div id="date" name="date" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">{{ dayjs(props.order[0].created_at).format('YYYY-MM-DD') }}</div>
                               </div>
                             </div>
                             <div class="p-2 w-full">
