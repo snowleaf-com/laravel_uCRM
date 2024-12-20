9422daea9ed3eb98b144991aa8dbeae3c35e2ec2
snowleaf-com <yy.dec5@gmail.com>
2024-12-21 08:20:53 +0900

    インデントを調整

--- src/laravel_uCRM/app/Http/Controllers/AnalysisController.php
+++ src/laravel_uCRM/app/Http/Controllers/AnalysisController.php
@@ -46,18 +46,18 @@ class AnalysisController extends Controller
 
     // 3. 会員毎のRFMランクを計算
     $rfmPrms = [
-        14,
-        28,
-        60,
-        90,
-        7,
-        5,
-        3,
-        2,
-        300000,
-        200000,
-        100000,
-        30000
+      14,
+      28,
+      60,
+      90,
+      7,
+      5,
+      3,
+      2,
+      300000,
+      200000,
+      100000,
+      30000
     ];
     $subQuery = DB::table($subQuery)
     ->selectRaw('
@@ -105,15 +105,15 @@ class AnalysisController extends Controller
     $rank = 5;
 
     for($i = 0; $i < 5; $i++) {
-        array_push($eachCount,
-            [
-                'rank' => $rank, 
-                'r' => $rCount[$i], 
-                'f' => $fCount[$i], 
-                'm'=> $mCount[$i]
-            ]
-        );
-        $rank--;
+      array_push($eachCount,
+        [
+          'rank' => $rank, 
+          'r' => $rCount[$i], 
+          'f' => $fCount[$i], 
+          'm'=> $mCount[$i]
+        ]
+      );
+      $rank--;
     }
 
     // dd($total, $eachCount, $rCount, $fCount, $mCount);
@@ -145,20 +145,22 @@ class AnalysisController extends Controller
     $endDate = '2022-08-10';
     
     // Orderモデルから１つの購入ごとの合計金額を出す
-    $subQuery = Order::betweenDate($startDate, $endDate)
-        ->groupBy('id')
-        ->selectRaw('id, customer_id, customer_name, SUM(subtotal) as totalPerPurchase');
+    $subQuery = 
+      Order::betweenDate($startDate, $endDate)
+      ->groupBy('id')
+      ->selectRaw('id, customer_id, customer_name, SUM(subtotal) as totalPerPurchase');
 
     // 購入ごとの合計金額（totalPerPurchase）をさらに合計し、顧客単位での「総購入額」を total として計算します。
-    $subQuery = DB::table($subQuery)
-        ->groupBy('customer_id')
-        ->selectRaw('customer_id, customer_name, sum(totalPerPurchase) as total')
-        ->orderBy('total', 'desc');
+    $subQuery = 
+      DB::table($subQuery)
+      ->groupBy('customer_id')
+      ->selectRaw('customer_id, customer_name, sum(totalPerPurchase) as total')
+      ->orderBy('total', 'desc');
 
     // 総購入額で降順に並び替えた後、MySQL変数（@row_num）を利用してランキング順位（row_num）を付与。
     DB::statement('set @row_num = 0');
     $subQuery = DB::table($subQuery)
-        ->selectRaw('@row_num:= @row_num +1 as row_num, 
+    ->selectRaw('@row_num:= @row_num +1 as row_num, 
     customer_id, customer_name, total');
 
     // $subQuery の行数（顧客数）を数えています。
@@ -175,9 +177,9 @@ class AnalysisController extends Controller
     $bindValues = [];
     $tempValue = 0;
     for ($i = 1; $i <= 10; $i++) {
-        array_push($bindValues, 1 + $tempValue); // 開始インデックス
-        $tempValue += $decile;
-        array_push($bindValues, 1 + $tempValue); // 終了インデックス
+      array_push($bindValues, 1 + $tempValue); // 開始インデックス
+      $tempValue += $decile;
+      array_push($bindValues, 1 + $tempValue); // 終了インデックス
     }
     // 例
     //   0 => 1
@@ -203,13 +205,14 @@ class AnalysisController extends Controller
 
     // デシル分割する decileに1〜10を入れてグループ分け
     DB::statement('set @row_num = 0;');
-    $subQuery = DB::table($subQuery)
-        ->selectRaw('
-    row_num,
-    customer_id,
-    customer_name,
-    total,
-    case
+    $subQuery = 
+      DB::table($subQuery)
+      ->selectRaw('
+        row_num,
+        customer_id,
+        customer_name,
+        total,
+        case
         when ? <= row_num and row_num < ? then 1
         when ? <= row_num and row_num < ? then 2
         when ? <= row_num and row_num < ? then 3
@@ -220,20 +223,22 @@ class AnalysisController extends Controller
         when ? <= row_num and row_num < ? then 8
         when ? <= row_num and row_num < ? then 9
         when ? <= row_num and row_num < ? then 10
-    end as decile
-    ', $bindValues);
+        end as decile
+      ', $bindValues);
 
     // デシルごとの集計｜各デシルの平均購入額・合計購入額
-    $subQuery = DB::table($subQuery)
-        ->groupBy('decile')
-        ->selectRaw('decile, round(avg(total)) as average,
-    sum(total) as totalPerGroup');
+    $subQuery =
+      DB::table($subQuery)
+      ->groupBy('decile')
+      ->selectRaw('decile, round(avg(total)) as average,
+      sum(total) as totalPerGroup');
 
     // @totalは総合計額
     // round〜の計算式は、各デシルの合計購入額が全体（@total）に占める割合を計算し、小数点第1位まで丸める。
     DB::statement("set @total = {$total} ;");
-    $data = DB::table($subQuery)
-        ->selectRaw('decile, average, totalPerGroup, @total,  round(100 * totalPerGroup / @total, 1) as totalRatio')->get();
+    $data = 
+      DB::table($subQuery)
+      ->selectRaw('decile, average, totalPerGroup, @total,  round(100 * totalPerGroup / @total, 1) as totalRatio')->get();
 
     // dd($data);
   }
